75555f3fbb9095d619045814811f71ddd22d36ae
amihaylov <anton.mihaylov7@gmail.com>
2015-09-30 22:46:05 +0300

    First public release.

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,60 +1,71 @@
 "use strict"
+	var questions = [
+		[ "Градът, в който живеете:", "София", "Пловдив", "Бургас", "Варна", "Стара Загора", "Русе", "button" ],
+		[ "Колко деца има във Вашето семейство?", "1", "2", "3", "4", "Повече от 4", "button" ],
+		[ "на каква възраст е най-малкото сред тях?", "Под 1 год.", "1-2год", "2-3год.", "3-4год.", "4-5год.", "Над 5 год.", "button" ],
+		[ "Използвали ли сте досега услугите на професионален детегледач и по какъв повод?", "Не", "Да, защото имам нужда от грижа за детето си, докато съм на работа", "Да, когато вечер излизам", "Да, когато пътувам и нямам възможност да бъда с детето си", "Да, когато съм вкъщи с детето си, но имам нужда от допълнителна помощ", "button" ],
+		[ "От какъв тип ангажираност на детегледача имате нужда?","На пълен работен ден/ до 10-12 часа на ден","На половин работен ден – до 4-6 часа на ден","Периодично – само при конкретна необходимост","Предпочитам да съжителства с нас","button"],
+		[ "Какъв вид дейности бихте поверили на професионален детегледач?","Приготвяне на храна за детето + хранене", "Грижа за хигиената", "Обучение/подготовка за училище", "Разходки навън ","Пътуване извън града","Игри с други деца навън или вкъщи","Придружаване/превоз (например, водене и вземане от училище/детска градина)","button"],
+		[ "Какви са опасенията Ви, когато поверявате детето си на чужди грижи?","Да не бъде заразено с болест (грип, шарка и др.)","Да не бъде наранено дори и по случайност","Да не бъде малтретирано (физически или емоционално)", "Да не бъдат игнорирани нуждите му","Да не повлияе отрицателно на дисциплината/режима му","Други инциденти, свързани с дома ни или личните ни вещи", "button"],
+		[ "Каква е сумата, която сте готови да отделите за професионален детегледач според вида ангажираност и дейностите, които включва услугата?", "под 5 лв./час", "5-10 лв./ час", "10-15 лв./ час", "15-20 лв./час","до 20-25 лв./час", "button" ],
+		[ "Моля, споделете с нас допълнителни коментари или опасения относно грижите на детегледача!", "text" ]
+
+	];
+
 	var circle = new ProgressBar.Circle('#bar', {
 		color: '#21bdd6',
 		strokeWidth: 3,
 		trailWidth: 1,
+		trailColor: '#ee4f6b',
+		trailWidth: 2,
 		duration: 1500,
 		text: {
 			value: '0'
+			// Change text color in quiz
+			// style: {
+			// 	color: '#fff'
+			// }
 		},
 		step: function(state, bar) {
-			bar.setText("Question " + (bar.value() * 5).toFixed(0) + "/5");
+			bar.setText((bar.value() * Number(questions.length+1)).toFixed(0) + "/" + Number(questions.length+1));
 		}
 	});
 
 	 // init stuff here
 	var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
-	var questions = [
-		[ "What is 10 + 4?", "12", "14", "16", "2" ],
-		[ "What is 20 - 9?", "7", "13", "11", "3" ],
-		[ "What is 7 x 3?", "21", "24", "25", "1" ],
-		[ "What is 8 / 2?", "10", "2", "4", "3" ]
-	];
+
+	var alphabet = ['а','б','в','г','д','е','ж','з','и','й'];
 	var answers = [];
-	// No longer needed as we now use jQuery
-	// function _(x){
-	// 	return document.getElementById(x);
-	// }
+
 	function renderQuestion(){
 		test = $("#test");
 		test.empty();
 
-		circle.animate((pos+1)/5, function(){
-			circle.animate((pos+1)/5);
+		circle.animate((pos+1)/Number(questions.length+1), function(){
+			circle.animate((pos+1)/Number(questions.length+1));
 		});
 
 		if(pos >= questions.length){
-			circle.animate(0.8, function(){
+			circle.animate((pos+1)/Number(questions.length+1), function(){
 				circle.animate(1);
 			});
-			// Showing correct answers
-			// test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-			// _("test_status").innerHTML = "Test Completed";
 			var pageBreak = $("<br>");
-			var h2 = $("<h2></h2>").text("Please submit your contact details and click submit:");
-			var names = $("<h3></h3>").text("Your first and last names:");
+			var h2 = $("<h2></h2>").text("Моля, въведете личните си данни, за да се свържем с Вас:");
+			var names = $("<h3></h3>").text("Име:");
 			var inputNames = $("<input>").attr({"type":"text","id":"names","value":"First and Last name"});
 			var email = $("<h3></h3>").text("Email:");
-			var inputEmail = $("<input>").attr({"type":"text","id":"email","value":"Valid email"});
-			var phone = $("<h3></h3>").text("Phone:");
+			var inputEmail = $("<input>").attr({"type":"email","id":"email","value":"Valid email"});
+			var phone = $("<h3></h3>").text("Телефон:");
 			var inputPhone = $("<input>").attr({"type":"text","id":"phone","value":"Phone number"});
 			test.append(h2).append(names).append(inputNames).append(pageBreak).append(email).append(inputEmail)
 				.append(pageBreak).append(phone).append(inputPhone).append(pageBreak).append(submit);
 
 			//Adding personal info at end of answers[]
-			answers.push(inputNames.val(),inputEmail.val(),inputPhone.val());
-			var submit = $("<button></button>").attr({"onclick":"SzoneApp.addClient(answers)"});
-			test.append(submit);
+			var legalNotice = $("<div></div>").text("Личните Ви данни ще бъдат използвани само за целите на услугата ни и няма да бъдат предоставени на трети лица! ")
+								.addClass("row legal-notice");
+			var submit = $("<button></button>").attr({"onclick":"submit()"}).text("Submit")
+							.addClass("btn btn-4 btn-4a btn-quiz btn-next");
+			test.append(legalNotice).append(submit);
 
 			pos = 0;
 			correct = 0;
@@ -63,44 +74,57 @@
 			return false;
 		}
 		question = questions[pos][0];
-		chA = questions[pos][1];
-		chB = questions[pos][2];
-		chC = questions[pos][3];
 
-		var h3 = $("<h3></h3>").text(question);
-		var buttonA = $("<button></button>").text(chA).attr({"name":"choices","onclick":"checkAnswer(1)"});	
-		var buttonB = $("<button></button>").text(chB).attr({"name":"choices","onclick":"checkAnswer(2)"});
-		var buttonC = $("<button></button>").text(chC).attr({"name":"choices","onclick":"checkAnswer(3)"});
-		test.append(h3).append(buttonA).append($("<br>")).append(buttonB).append($("<br>")).append(buttonC);
-		
-		//test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-	}
-	function checkAnswer(answer){
-		// choices = document.getElementsByName("choices");
-		// for(var i=0; i<choices.length; i++){
-		// 	if(choices[i].checked){
-		// 		choice = choices[i].value;
-				switch(answer){
-					default:
-						console.log("Error in choices!")
-					case(1):
-						answers.push(questions[pos][1]);
-						break;
-					case(2):
-						answers.push(questions[pos][2]);
-						break;
-					case(3):
-						answers.push(questions[pos][3]);
-						break;
+		var spanPos = $("<span></span>").text(pos+1+".").addClass("quiz-span");
+		var h3 = $("<h3></h3>").text(question).prepend(spanPos);
+		test.append(h3);
+		switch (questions[pos][questions[pos].length-1]){
+			default:
+			case("button"):
+				for(var i=1;i<questions[pos].length-1; i+=1){
+					var strFunction = "checkAnswer("+i+")";
+					var spanLetter = $("<span></span>").text(alphabet[i-1]).addClass("quiz-span");
+					var buttonQuiz = $("<button></button>").text(questions[pos][i])
+									.attr({"name":"choices","onclick":strFunction});
+					buttonQuiz.prepend(spanLetter);
+					buttonQuiz.addClass("btn btn-6 btn-6c btn-quiz");
+					//For cities different than Sofia
+					if(i>1 && pos===0)
+						buttonQuiz.addClass("no-sofia")
+							.attr({"title":"За момента услугата на SitterZone се предлага само в София-град. Скоро ще предоставам нашата грижа и във Вашето населено място! "})
+							.addClass("text-nosofia");
+					
+					test.append(buttonQuiz).append($("<br>"));
+					//TODO Add previous button question and logic
 				}
-				//console.log(answers);
-		// 	}
-		// }
-		// Counting correct answers
-		// if(choice == questions[pos][4]){
-		// 	correct++;
-		// }
+			break;
+			case("text"):
+				var textArea = $("<textarea></textarea>").attr({"rows": 4,"cols": 50, "maxlength": 500, "id": "quiz-text"})
+								.addClass("quiz-textarea");
+				test.append(textArea).append($("<br>"));
+				answers.push()
+				var strFunction = "checkAnswer(\"text\")";
+				var button = $("<button></button>").addClass("btn btn-4 btn-4a btn-quiz btn-next")
+							.attr({"name":"choices","onclick":strFunction}).text("НАПРЕД");
+				test.append(button).append($("<br>"));
+			break;
+		}
+		
+	}
+
+	function checkAnswer(answer){
+				if(typeof answer === "number")	
+					answers.push(questions[pos][answer]);
+				else
+					answers.push($("#quiz-text").val());
+
 		pos++;
 		renderQuestion();
 	}
+
+	function submit(){
+		answers.push($("#names").val(),$("#email").val(),$("#phone").val());
+		SzoneApp.addClient(answers);
+	}
+
 	window.addEventListener("load", renderQuestion, false);
